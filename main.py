26484d2a2ac6a32b6fca676e4b689ae5e5d0e7c1
Om kulkarni <justmeaprogrammer@gmail.com>
2026-04-08 04:51:20 +0530

    Data Validation done

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from networksecurity.logging.logger import logging
 from networksecurity.entity.config_entity import DataIngestionConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 
+from networksecurity.components.data_validation import DataValidation
+from networksecurity.entity.config_entity import DataValidationConfig
+from networksecurity.entity.artifact_entity import DataIngestionArtifact 
+
 import sys
 
 
@@ -14,7 +18,16 @@ if __name__=="__main__":
         dataingestion=DataIngestion(data_ingestion_config=dataingestionconfig)
         logging.info("Initiating Data Ingestion")
         dataingestionartifact=dataingestion.initiate_data_ingestion()
+        logging.info("data initiation complpeet")
         print(dataingestionartifact)
+        
+        dataValidationConfig=DataValidationConfig(trainigpiplelineconfig)
+        datavalidationobject=DataValidation(data_ingestion_artifact=dataingestionartifact,
+                                            data_validation_config=dataValidationConfig)
+        logging.info("INITIATE DATA INGESTION !")
+        data_validation_artifact=datavalidationobject.initiat_data_validation()
+        logging.info("data validation completed")
+        print(data_validation_artifact)        
     
     except Exception as e:
         raise NetworkSecurityException(e,sys)
--- networksecurity/constant/trainig_pipeline/__init__.py
+++ networksecurity/constant/trainig_pipeline/__init__.py
@@ -9,8 +9,11 @@ ARTIFACT_DIR:str="Artifacts"
 FILE_NAME:str="phisingData.csv"
 TRAIN_FILE_NAME:str="train.csv"
 TEST_FILE_NAME:str="test.csv"
+SCHEMA_FILE_PATH:str = os.path.join("data_schema","schema.yaml")
 
-
+"""   
+     Data Ingestion Related CONSTANTS start with DATA_INGESTION
+"""
 
 DATA_INGESTION_COLLECTION_NAME:str ="NetworkData"
 DATA_INGESTION_DATABASE_NAME:str ="OmKulkarni"
@@ -18,3 +21,13 @@ DATA_INGESTION_DIR_NAME:str ="data_ingestion"
 DATA_INGESTION_FEATURE_STORE_DIR:str ="feature_store"
 DATA_INGESTION_INGESTED_DIR:str ="ingested"
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO:float=0.2
+
+"""   
+     Data Validation Related CONSTANTS start with DATA_VALIDATION
+"""
+
+DATA_VALIDATION_DIR_NAME:str ="data_validation"
+DATA_VALIDATION_VALID_DIR:str ="validated"
+DATA_VALIDATION_INVALID_DIR:str ="invalid"
+DATA_VALIDATION_DRIFT_REPORT_DIR:str ="drift_report"
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME:str ="report.yaml"
